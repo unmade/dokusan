12b36c2f41ffd20ef3418a06298b9d82fdfc6225
Aleksei Maslakov <lesha.maslakov@gmail.com>
2019-12-26 14:03:39 +0300

    Remove magic numbers in generators

--- src/dokusan/generators.py
+++ src/dokusan/generators.py
@@ -4,6 +4,8 @@ from typing import List
 from dokusan import exceptions, solvers, stats
 from dokusan.entities import BoxSize, Cell, Position, Sudoku
 
+MAX_ITERATIONS = 300
+
 
 def random_sudoku(min_rank: int = 150, box_size: BoxSize = BoxSize(3, 3)) -> Sudoku:
     sudoku = Sudoku(*_random_initial_cells(box_size), box_size=box_size)
@@ -12,7 +14,7 @@ def random_sudoku(min_rank: int = 150, box_size: BoxSize = BoxSize(3, 3)) -> Sud
     best_sudoku = Sudoku(*solution.cells(), box_size=box_size)
     best_rank = 0
 
-    iterations = max(50, min(min_rank, 300))
+    iterations = min(min_rank, MAX_ITERATIONS)
     for i in range(iterations):
         size = random.randint(1, 2)
         rows = [random.randint(0, solution.size - 1) for _ in range(size)]
@@ -43,24 +45,27 @@ def _random_initial_cells(box_size: BoxSize) -> List[Cell]:
         for i in range(box_size.width)
     ]
 
-    while (row_values := random.sample(all_values.difference(box_values[0]), k=3)) and (
-        sorted(row_values) == sorted(box_values[1])
-        or sorted(row_values) == sorted(box_values[2])
-    ):
-        continue
-    row_values += random.sample(all_values.difference(box_values[0], row_values), k=3)
+    while True:
+        row_values = random.sample(all_values - set(box_values[0]), k=box_size.length)
+        used_values = [sorted(box_values[i]) for i in range(1, box_size.width)]
+        if sorted(row_values) not in used_values:
+            break
+
+    row_values += random.sample(
+        all_values.difference(box_values[0], row_values), k=box_size.length
+    )
 
     return [
         Cell(
-            position=Position(row=i, column=j, box=box_size.sequential(i, j),),
+            position=Position(row=i, column=j, box=box_size.sequential(i, j)),
             value=box_values[i][j],
         )
         for i in range(box_size.width)
         for j in range(box_size.length)
     ] + [
         Cell(
-            position=Position(row=0, column=i, box=box_size.sequential(0, i),),
+            position=Position(row=0, column=i, box=box_size.sequential(0, i)),
             value=value,
         )
-        for i, value in enumerate(row_values, start=3)
+        for i, value in enumerate(row_values, start=box_size.length)
     ]
